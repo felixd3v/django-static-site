2c28cb8bef29b3d33b31d26fb28e15a99dd008bd
felixd3v <fmhcde@gmail.com>
2022-11-03 14:06:54 -0500

    add About Page

--- sections/urls.py
+++ sections/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AboutPageView
+ 
 
 urlpatterns = [
+    path("about/", AboutPageView.as_view(), name="about"),
     path("", HomeView.as_view(), name="home"),
+
 ]
